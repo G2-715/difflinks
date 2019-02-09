c1a3311b72121f4400f9082f2f349751b87fb8a8
whiled0s <whiled0s@yandex.ru>
2019-02-10 00:13:07 +0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.join(__dirname, "src/index.js"),
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: [
+        "style-loader", 
+        "css-loader", 
+        "sass-loader" 
+      ]
+    }]
+  },
+  plugins: [
+    new CopyWebpackPlugin([
+      { from: "./src/index.html", to: "./index.html" }
+    ])
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000
+  }
+}
